00335be76ddd14f54f9c5197b5b1f9dcc6a6381a
Graham Kelly <gkgoat6700@gmail.com>
2025-12-05 23:13:06 -0500

    cbor stuff

--- packages/common-impl/ui.ts
+++ packages/common-impl/ui.ts
@@ -1,6 +1,7 @@
 import { ChallengeFactory } from "@portal-solutions/pasqi-snap";
 import { _Uint8Array } from "@portal-solutions/pasqi-snap";
 import { AuthenticatorChallengeRaw, toToSignArrayBuffer } from "@portal-solutions/pasqi-passkey";
+import { encode } from "cbor2";
 
 export class UI {
     readonly #success: (result: UIResult) => void;
@@ -20,7 +21,7 @@ export class UI {
     get params() {
         return this.#params;
     }
-    readonly #sign: (alg: (input: Uint8Array) => Uint8Array | Promise<Uint8Array>) => Promise<UIResult> = async (alg) => {
+    readonly #sign: (alg: UIAlg) => Promise<UIResult> = async (alg) => {
         try {
             const result = await sign(this.#params, alg);
             this.#success(result);
@@ -39,19 +40,58 @@ export interface UIResult {
 }
 export interface UIParams {
     user: PublicKeyCredentialUserEntity | { anonymousVerifyingKey: BufferSource };
-    toSign: {challenge: AuthenticatorChallengeRaw};
+    toSign: { challenge: AuthenticatorChallengeRaw & { type: "webauthn.get" } } | { create: AuthenticatorChallengeRaw & { type: "webauthn.create" } };
     id: BufferSource;
 }
-export async function sign(params: UIParams, alg: (input: Uint8Array) => Uint8Array | Promise<Uint8Array>): Promise<UIResult> {
-    const toSign = 'challenge' in params.toSign ? toToSignArrayBuffer(params.toSign.challenge) : toToSignArrayBuffer(params.toSign);
-    const signature = await alg(toSign);
-    const handle = ('anonymousVerifyingKey' in params.user ? (params.user.anonymousVerifyingKey) : params.user.id);
-    return {
-        signed: {
-            clientDataJSON: new Uint8Array(toSign).buffer,
-            authenticatorData: new Uint8Array().buffer, // Empty for passkey signatures
-            signature: signature.slice().buffer,
-            userHandle: 'buffer' in handle ? new Uint8Array(handle.buffer).slice().buffer : new Uint8Array(handle).buffer
-        }
-    };
+export interface UIAlg { 
+    rpIdHash: Uint8Array;
+    parentFlags: number;
+    verifyingKey: any;
+    alg: (input: Uint8Array) => Uint8Array | Promise<Uint8Array>;
+}
+export async function sign(params: UIParams, {rpIdHash,parentFlags,verifyingKey,alg}: UIAlg): Promise<UIResult> {
+    const handle = (handle => 'buffer' in handle ? new Uint8Array(handle.buffer).slice().buffer : new Uint8Array(handle).buffer)(('anonymousVerifyingKey' in params.user ? (params.user.anonymousVerifyingKey) : params.user.id));
+    const childFlags = 0x18 | (parentFlags & 5);
+    if ('challenge' in params.toSign) {
+
+        const toSign = toToSignArrayBuffer({...params.toSign.challenge});
+        const signature = await alg(toSign);
+        const authdata = new _Uint8Array([...rpIdHash, childFlags, 0, 0, 0, 0]);
+        return {
+            signed: {
+                clientDataJSON: new Uint8Array(toSign).buffer,
+                authenticatorData: new Uint8Array(authdata).buffer, // Empty for passkey signatures
+                signature: signature.slice().buffer,
+                userHandle: handle,
+            }
+        };
+    } else if ('create' in params.toSign) {
+        const toSign = toToSignArrayBuffer({...params.toSign.create});
+        const signature = await alg(toSign);
+        const authdata = new _Uint8Array([...rpIdHash, 0x40 | childFlags, 0, 0, 0, 0, ...encode(verifyingKey)]);
+        return {
+            signed: {
+                clientDataJSON: new Uint8Array(toSign).buffer,
+                attestationObject: encode({
+                    fmt: "none",
+                    authData: authdata,
+                    attStmt: {}
+                }).slice().buffer,
+                getPublicKey() {
+                    return null; //TODO: parse CBOR to get public key
+                },
+                getAuthenticatorData() {
+                    return authdata.slice().buffer;
+                },
+                getPublicKeyAlgorithm() {
+                    return null; //TODO: parse CBOR to get algorithm
+                },
+                getTransports() {
+                    return [];
+                },
+            }
+        };
+    } else {
+        throw new Error("Unsupported toSign parameter");
+    }
 }
