5460e427c2048e93952179f17d3fcef09b9d8a9a
Graham Kelly <gkgoat6700@gmail.com>
2025-12-05 22:35:21 -0500

    work on ui interface

--- packages/common-impl/ui.ts
+++ packages/common-impl/ui.ts
@@ -1,4 +1,6 @@
-import { ChallengeFactory } from "../snap/api";
+import { ChallengeFactory } from "@portal-solutions/pasqi-snap";
+import { _Uint8Array } from "@portal-solutions/pasqi-snap";
+import { AuthenticatorChallengeRaw, toToSignArrayBuffer } from "@portal-solutions/pasqi-passkey";
 
 export class UI {
     readonly #success: (result: UIResult) => void;
@@ -18,12 +20,38 @@ export class UI {
     get params() {
         return this.#params;
     }
+    readonly #sign: (alg: (input: Uint8Array) => Uint8Array | Promise<Uint8Array>) => Promise<UIResult> = async (alg) => {
+        try {
+            const result = await sign(this.#params, alg);
+            this.#success(result);
+            return result;
+        } catch (err) {
+            this.#failure(err as Error);
+            throw err;
+        }
+    }
+    get sign() {
+        return this.#sign;
+    }
 }
 export interface UIResult {
-
+    signed: AuthenticatorAssertionResponse | AuthenticatorAttestationResponse;
 }
 export interface UIParams {
-    user: PublicKeyCredentialUserEntity | { anonymousKey: BufferSource };
-    toSign: Uint8Array<BufferSource & ArrayBufferLike>;
+    user: PublicKeyCredentialUserEntity | { anonymousVerifyingKey: BufferSource };
+    toSign: {challenge: AuthenticatorChallengeRaw};
     id: BufferSource;
+}
+export async function sign(params: UIParams, alg: (input: Uint8Array) => Uint8Array | Promise<Uint8Array>): Promise<UIResult> {
+    const toSign = 'challenge' in params.toSign ? toToSignArrayBuffer(params.toSign.challenge) : toToSignArrayBuffer(params.toSign);
+    const signature = await alg(toSign);
+    const handle = ('anonymousVerifyingKey' in params.user ? (params.user.anonymousVerifyingKey) : params.user.id);
+    return {
+        signed: {
+            clientDataJSON: new Uint8Array(toSign).buffer,
+            authenticatorData: new Uint8Array().buffer, // Empty for passkey signatures
+            signature: signature.slice().buffer,
+            userHandle: 'buffer' in handle ? new Uint8Array(handle.buffer).slice().buffer : new Uint8Array(handle).buffer
+        }
+    };
 }
